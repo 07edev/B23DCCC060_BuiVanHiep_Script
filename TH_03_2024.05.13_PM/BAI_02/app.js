20e6374f7e0b442bf471bacba3909a0559026cc6
07edev <164862279+07edev@users.noreply.github.com>
2024-05-13 15:20:32 +0700

    Update app.js

--- TH_03_2024.05.13_PM/BAI_02/app.js
+++ TH_03_2024.05.13_PM/BAI_02/app.js
@@ -1,15 +1,9 @@
 $(document).ready(function(){
     $("#addItemBtn").click(function(){
-        // Lấy giá trị từ ô input
         var newItemValue = $("#newItemInput").val().trim();
-
-        // Kiểm tra xem ô input có rỗng hay không
         if(newItemValue !== '') {
-            // Tạo một phần tử mới cho danh sách
             var newItem = $("<li>").text(newItemValue);
-            // Thêm phần tử mới vào danh sách
             $("#itemList").append(newItem);
-            // Xoá trắng ô input
             $("#newItemInput").val('');
         }
     });
